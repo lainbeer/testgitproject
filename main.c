89c7a4670b7d14eb03853e6d484ac359ec984a32
Alex <sungchan.lee1@gamil.com>
2015-06-22 18:57:26 +0900

    - add test code

--- main.c
+++ main.c
@@ -8,10 +8,17 @@
 /*
  * Function main
  */
-int main ( void )
+int main ( int argc, char *argv[] )
 {
+  int i ;
+
   /* Insert commment!! */
   /* */
+
+  if ( argc > 1 )
+  {
+    for ( i = 1; i < argc; i++ ) printf ( ">argv[%d]=%s\n", i, argv[i] ) ;
+  }
 
   printf ( "Version: %d.%d.%s\r\n", test_vsn_major, test_vsn_minor, test_vsn_string ) ;
 

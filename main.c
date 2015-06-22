e2fe7ef7a1c12eaebe452acd8069f55be07997b5
Alex <sungchan.lee1@gamil.com>
2015-06-22 15:06:48 +0900

    - add version file - add build script

--- main.c
+++ main.c
@@ -2,13 +2,17 @@
   This is test main.c
 ---------------------------------------------------------------*/
 #include "stdio.h"
+#include "test_vsn.h"
+
 
 /*
  * Function main
  */
-void main ( void )
+int main ( void )
 {
   /* Insert commment!! */
+
+  printf ( "Version: %d.%d.%s\r\n", test_vsn_major, test_vsn_minor, test_vsn_string ) ;
 
   return 0;
 }
